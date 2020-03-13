112035e964e82fc37b2d3d7b139b113e56098e3a
Isvvc <lyons@tuta.io>
2020-03-13 15:14:15 -0600

    Create Xcode project and basic data model

---
+++ Food Tracker/Food Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Food Tracker
+//
+//  Created by Isaac Lyons on 3/13/20.
+//  Copyright © 2020 Isaac Lyons. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
