82d9d3b838cf21d9350fddaadd3f31f6d695f6fb
Isvvc <lyons@tuta.io>
2020-03-30 22:13:16 -0600

    Implement UI for setting goals

---
+++ Food Tracker/Food Tracker/Settings/GoalView.swift
@@ -0,0 +1,52 @@
+//
+//  GoalView.swift
+//  Food Tracker
+//
+//  Created by Isaac Lyons on 3/30/20.
+//  Copyright © 2020 Isaac Lyons. All rights reserved.
+//
+
+import SwiftUI
+
+struct GoalView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    @State private var date = Date()
+    @State private var amount: Int16 = 18
+    
+    var goal: Goal?
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    DatePicker(
+                        "Start Date",
+                        selection: $date,
+                        displayedComponents: .date)
+                    
+                    Stepper(value: $amount, in: 0...64) {
+                        Text("\(amount) Fists")
+                    }
+                }
+                
+                Section {
+                    Button("Save") {
+                        //<#code#>
+                    }
+                }
+            }
+            .navigationBarTitle("Set Goal")
+            .navigationBarItems(leading: Button(action: { self.presentationMode.wrappedValue.dismiss() }) {
+                Text("Cancel")
+            })
+        }
+    }
+}
+
+struct GoalView_Previews: PreviewProvider {
+    static var previews: some View {
+        GoalView()
+    }
+}
--- Food Tracker/Food Tracker/Settings/GoalsView.swift
+++ Food Tracker/Food Tracker/Settings/GoalsView.swift
@@ -9,13 +9,52 @@
 import SwiftUI
 
 struct GoalsView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Goal.entity(), sortDescriptors: [NSSortDescriptor(key: "startDate", ascending: true)]) var goals: FetchedResults<Goal>
+    
+    @State private var showingGoal = false
+    @State private var selectedGoal: Goal?
+    
+    let dateFormatter: DateFormatter = {
+        let value = DateFormatter()
+        value.dateFormat = "EEEE, MMM d"
+        return value
+    }()
+    
+    var addGoalButtion: some View {
+        Button("Set new goal") {
+            self.showingGoal = true
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(goals, id: \.self) { goal in
+                Button(action: {
+                    self.selectedGoal = goal
+                    self.showingGoal = true
+                }) {
+                    HStack {
+                        Text("Starting \(self.dateFormatter.string(from: goal.startDate ?? Date()))")
+                        Spacer()
+                        Text("eat \(goal.amount)")
+                    }
+                }
+            }
+        }
+        .navigationBarTitle("Goals")
+        .navigationBarItems(trailing: addGoalButtion)
+        .sheet(isPresented: $showingGoal) {
+            GoalView()
+                .environment(\.managedObjectContext, self.moc)
+        }
     }
 }
 
 struct GoalsView_Previews: PreviewProvider {
     static var previews: some View {
-        GoalsView()
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        return GoalsView()
+            .environment(\.managedObjectContext, context)
     }
 }
